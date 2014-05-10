7a8940894df4fbcde678d9849a7707440855d616
Tay <me@taylorrome.ro>
2014-05-10 12:08:46 -0600

    form class improved can have values set

--- Forms.js
+++ Forms.js
@@ -1,23 +1,119 @@
 define(['altair/facades/declare',
-        'altair/mixins/_DeferredMixin',
-        'apollo/Schema'
+        'altair/Lifecycle',
+        'apollo/Schema',
+        'altair/facades/mixin',
+        'altair/events/Emitter',
+        'lodash'
 
 ], function (declare,
-             _DeferredMixin,
-             Schema) {
+             Lifecycle,
+             Schema,
+             mixin,
+             Emitter,
+             _) {
 
-    return declare([_DeferredMixin], {
+    return declare([Lifecycle, Emitter], {
 
-        form: function (schema) {
+        _forms: {}, //form cache
 
-            var _schema = schema;
+        startup: function (options) {
 
-            if(!_schema.isInstanceOf || !_schema.isInstanceOf(Schema)) {
-                _schema = this.nexus('cartridges/Apollo').createSchema(schema);
+
+            //hook into requests to see if forms were submitted
+            this.on('titan:Alfred::did-receive-request').then(this.hitch('onDidReceiveRequest'));
+
+
+            return this.inherited(arguments);
+
+        },
+
+        /**
+         * Get a form yo!
+         *
+         * @param options
+         * @param values
+         * @returns {*}
+         */
+        form: function (options) {
+
+            var _options = options || {},
+                schema = _options.schema,
+                values = _options.values,
+                formId  = _options.id,
+                dfd;
+
+            if(!formId) {
+                dfd = new this.Deferred();
+                dfd.reject(new Error('Forms need an id and schema'));
             }
 
-            return this.forge('lib/Form', _schema);
+            else if(_.has(this._forms, formId)) {
 
+                dfd = this.when(this._forms[formId]);
+
+            } else {
+
+                if(!schema) {
+                    dfd = new this.Deferred();
+                    dfd.reject(new Error('Form is invalid (no schema). This can happen because a form needs to be rendered once before it can be submitted.'));
+
+                } else {
+
+                    //turn generic object into schema
+                    if(!schema.isInstanceOf || !schema.isInstanceOf(Schema)) {
+                        schema = this.nexus('cartridges/Apollo').createSchema(schema);
+                    }
+
+                    dfd = this.forge('lib/Form', _options ).then(this.hitch(function (form) {
+                        this._forms[formId] = form;
+                        return form;
+                    }));
+
+                }
+
+
+            }
+
+
+            return dfd.then(function (form) {
+
+                if(values) {
+                    return form.populate(values);
+                } else {
+                    return form;
+                }
+
+            });
+
+        },
+
+        /**
+         * When a request was received, lets see about form submissions.
+         *
+         * @param e
+         */
+        onDidReceiveRequest: function (e) {
+
+            var r       = e.get('request'),
+                formId  = r.get('altair-form-id');
+
+
+            if(formId) {
+
+                return this.form({
+                    id: formId,
+                    values: mixin(r.query(), r.post())
+                }).then(this.hitch(function (form) {
+
+                    var data        = e.data;
+                        data.form   = form;
+
+                    return this.emit('did-submit-form', data);
+
+
+                }));
+
+            }
 
         }
 
--- widgets/form/Form.js
+++ widgets/form/Form.js
@@ -13,13 +13,29 @@ define(['altair/facades/declare',
 
     return declare([Lifecycle, _HasSchemaMixin], {
 
-        getViewValues: function () {
+        context: function () {
 
             var schema      = this.get('formSchema'),
+                values      = this.get('formValues'),
                 form;
 
             //load a form using the schema
-            return this.parent.form(schema).then(this.hitch(function (_form) {
+            return this.parent.form({
+                id: this.instanceId + '-form',
+                schema: schema
+            }).then(this.hitch(function (form) {
+
+                //pass through settings
+                form.method = this.get('method');
+
+                //populate the form if necessary
+                if(values) {
+                    return form.populate(values);
+                } else {
+                    return form;
+                }
+
+            })).then(this.hitch(function (_form) {
 
                 //we'll need the form for later
                 form = _form;
@@ -35,38 +51,72 @@ define(['altair/facades/declare',
                     colClass    = this.get('colClass', 'col-md-%d'),
                     currentCol  = 0,
                     rendered    = {},
-                    properties  = form.renderableProperties();
+                    hiddenProps = [],
+                    properties  = form.renderableProperties(),
+                    visibleProps = [];
 
-                //split props into rows and drop in classes and templates, then render templates
+                //split props into rows and drop in classes, templates, and html attributes, then render templates
                 _.each(properties, function (prop) {
 
-                    var cols = _.has(prop, 'form') ? prop.form.col : maxCols;
+                    var cols = _.has(prop, 'form') && _.has(prop.form, 'col') ? prop.form.col : maxCols,
+                        isHidden = form.isHidden(prop.name);
 
-                    if(cols + currentCol > maxCols) {
-                        row = row + 1;
-                        currentCol = cols;
-                    } else {
-                        currentCol += cols;
+                    //if it's not hidden, calculate column and row
+                    if(!isHidden) {
+
+                        //add to list op visible props
+                        visibleProps.push(prop);
+
+                        if(cols + currentCol > maxCols) {
+                            row = row + 1;
+                            currentCol = cols;
+                        } else {
+                            currentCol += cols;
+                        }
+
+                        if(!rows[row]) {
+                            rows[row] = [];
+                        }
+
+                        //classname of wrapper
+                        prop.className  = sprintf(colClass, cols);
+
+                        //add to rows
+                        rows[row].push(prop);
+
+
+
+                    }
+                    //if it is hidden, add to hiddenProps and remove from props so we don't double render
+                    else {
+                        hiddenProps.push(prop);
                     }
 
-                    if(!rows[row]) {
-                        rows[row] = [];
-                    }
-
-                    prop.className  = sprintf(colClass, cols);
+                    //always pass the template
                     prop.template   = templates[prop.name];
 
-                    rows[row].push(prop);
+                    //render html attributes and lastly, the property
+                    rendered[prop.name] = form.renderPropertyAttributes(prop).then(this.hitch(function (attribs) {
 
-                    //give rendered version
-                    rendered[prop.name] = this.parent.render(prop.template, prop);
+                        prop.attributes = attribs;
+                        prop.value      = form.get(prop.name);
+
+                        if(_.isUndefined(prop.value)) {
+                            prop.value = '';
+                        }
+
+                        return this.parent.render(prop.template, prop);
+                    }));
 
                 }, this);
 
+
                 return all({
+                    form: form, //so we have action, method, enctype, and other form specific properties
                     rows: rows, //properties in rows
                     rendered: all(rendered), //already rendered form elements
-                    properties: properties //all properties in the order they are in the schema
+                    properties: visibleProps, //all properties in the order they are in the schema
+                    hiddenProperties: hiddenProps
                 });
 
             }));
